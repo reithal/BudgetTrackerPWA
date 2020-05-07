262acbff88d83f2aefad47189f7203aaf0acc447
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-05-06 23:14:14 -0400

    Heroku setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://budgetUser:RandomBudgetPW1@ds041821.mlab.com:41821/heroku_f7drb1fd", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
