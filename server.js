78cf926264e8efee7022734ef9bc8e6df8b4a733
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2025-03-05 23:28:46 -0500

    Remove uneeded Dependencies

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
